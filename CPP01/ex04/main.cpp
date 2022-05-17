73fd41fe8aad12803292f42e7080487e221a7361
seongeun42 <gkapf838@gmail.com>
2022-05-17 23:25:41 +0900

    CPP01/ex04

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main(int argc, char* argv[])
+{
+	if (argc != 4)
+	{
+		std::cerr << "Program needs 3 arguments (filename, string1, string2)\n";
+		return 1;
+	}
+
+	char* file = argv[1];
+	std::string target = argv[2];
+	std::string replace = argv[3];
+
+	std::string line;
+	std::ifstream ifs;
+	ifs.open(file);
+	if (ifs.fail())
+	{
+		std::cerr << file << " open failed!\n";
+		return 1;
+	}
+
+	std::ofstream ofs;
+	ofs.open("output.txt");
+	if (ofs.fail())
+	{
+		std::cerr << "output.txt open failed!\n";
+		return 1;
+	}
+
+	while (!ifs.eof())
+	{
+		std::getline(ifs, line);
+		size_t idx = line.find(target);
+		if (idx != std::string::npos)
+		{
+			line.erase(idx, target.length());
+			line.insert(idx, replace);
+		}
+		ofs << line << '\n';
+	}
+
+	ifs.close();
+	ofs.close();
+
+	return 0;
+}
