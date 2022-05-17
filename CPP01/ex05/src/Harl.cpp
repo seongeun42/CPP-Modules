86b98fa42eb7f155971bd674f2727b6a7fc1d237
seongeun42 <gkapf838@gmail.com>
2022-05-18 00:25:12 +0900

    CPP01/ex05 - 문구 수정

--- CPP01/ex05/src/Harl.cpp
+++ CPP01/ex05/src/Harl.cpp
@@ -10,18 +10,18 @@ Harl::~Harl()
 
 void Harl::debug()
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.I really do!\n";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!\n";
 }
 
 void Harl::info()
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn¡¯t put "
-		<< "enough bacon in my burger!If you did, I wouldn¡¯t be asking for more!\n";
+	std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn't put "
+		<< "enough bacon in my burger!\nIf you did, I wouldn't be asking for more!\n";
 }
 
 void Harl::warning()
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I¡¯ve been coming for "
+	std::cout << "I think I deserve to have some extra bacon for free.\nI've been coming for "
 		<< "years whereas you started working here since last month.\n";
 }
 
