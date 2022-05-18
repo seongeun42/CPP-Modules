036f169d84a45eaa9130e8a970931cc470b09ebd
seongeun42 <42.4.seongele@gmail.com>
2022-05-18 17:15:24 +0900

    CPP00/ex00 - megaphone 수정

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: seongele <seongele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:50:47 by seongele          #+#    #+#             */
-/*   Updated: 2022/05/18 15:50:48 by seongele         ###   ########.fr       */
+/*   Updated: 2022/05/18 17:15:01 by seongele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char* argv[])
 	{
 		for (int i = 1; argv[i]; i++)
 			for (int j = 0; argv[i][j]; j++)
-				std::cout << char(toupper(argv[i][j]));
+				std::cout << (char)std::toupper(argv[i][j]);
 		std::cout << '\n';
 	}
 	return 0;
