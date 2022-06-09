f9b065523aca6a49e5676f220b601ebc48182a1c
seongeun42 <gkapf838@gmail.com>
2022-06-09 23:09:53 +0900

    CPP02/ex03

---
+++ CPP02/ex03/src/bsp.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongele <seongele@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 20:43:19 by seongele          #+#    #+#             */
+/*   Updated: 2022/06/07 20:43:19 by seongele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Point.hpp"
+
+static int calcTriangle(Point const v1, Point const v2, Point const v3)
+{
+	int ans = v1.getX().toFloat() * (v2.getY().toFloat() - v3.getY().toFloat())
+		+ v2.getX().toFloat() * (v3.getY().toFloat() - v1.getY().toFloat())
+		+ v3.getX().toFloat() * (v1.getY().toFloat() - v2.getY().toFloat());
+	return ans < 0 ? -ans : ans;
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point)
+{
+	int target = calcTriangle(a, b, c);
+	int aa = calcTriangle(point, b, c);
+	int bb = calcTriangle(a, point, c);
+	int cc = calcTriangle(a, b, point);
+	return target == aa + bb + cc;
+}
---
+++ CPP02/ex03/src/main.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongele <seongele@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 15:58:00 by seongele          #+#    #+#             */
+/*   Updated: 2022/06/09 23:07:43 by seongele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Point.hpp"
+
+int main()
+{
+	// 생성자 테스트
+	std::cout << "***** 생성자 테스트 *****\n";
+	Point a, b(2, 0), c(0, 2), d(b);
+
+	std::cout << "디폴트 생성자	: " << "a: " << &a << ", " << a << std::endl;
+	std::cout << "float 생성자	: " << "b: " << &b << ", " << b << std::endl;
+	std::cout << "float 생성자	: " << "c: " << &c << ", " << c << std::endl;
+	std::cout << "복사 생성자	: " << "d: " << &d << ", " << d << std::endl;
+
+	
+	// 대입 연산자 오버로딩 테스트
+	std::cout << "\n\n***** 대입 연산자 오버로딩 *****\n";
+	a = b;
+	std::cout << "a: " << &a << ", " << a << std::endl;
+
+
+	// 삼각형 내부의 점 판별 테스트
+	std::cout << "\n\n***** 삼각형 내부의 점 판별 함수 bsp 테스트 *****\n";
+
+	Point e(0, 0);
+	Point target1(1, 0);
+	Point target2(2, 1);
+
+	std::cout << "(0, 0), (2, 0), (0, 2)를 꼭짓점으로 가지는 삼각형이 있을 때,\n";
+	if (bsp(e, b, c, target1))
+		std::cout << target1 << "은 삼각형 내부의 점입니다!\n";
+	else
+		std::cout << target1 << "은 삼각형 내부의 점이 아닙니다!\n";
+
+	if (bsp(e, b, c, target2))
+		std::cout << target2 << "은 삼각형 내부의 점입니다!\n";
+	else
+		std::cout << target2 << "은 삼각형 내부의 점이 아닙니다!\n";
+	
+	return 0;
+}
