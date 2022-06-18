52f35c87f6a0e4570db9ee047ea5ae9a769ba99b
seongeun42 <42.4.seongele@gmail.com>
2022-06-18 10:35:58 +0900

    [modify] CPP00/ex00 - endl

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: seongele <seongele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 15:50:47 by seongele          #+#    #+#             */
-/*   Updated: 2022/05/18 17:15:01 by seongele         ###   ########.fr       */
+/*   Updated: 2022/06/18 10:35:21 by seongele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int argc, char* argv[])
 		for (int i = 1; argv[i]; i++)
 			for (int j = 0; argv[i][j]; j++)
 				std::cout << (char)std::toupper(argv[i][j]);
-		std::cout << '\n';
+		std::cout << std::endl;
 	}
 	return 0;
 }
